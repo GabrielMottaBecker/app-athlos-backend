ddc8981bcdccfc6a0b83797442d878cefff40cb1
Aguiando <aguiar98.daniel@gmail.com>
2026-06-02 20:31:40 -0300

    Fix: token

--- services/user-auth/drizzle/0001_seed_admin_user.sql
+++ services/user-auth/drizzle/0001_seed_admin_user.sql
@@ -1,34 +1,17 @@
 INSERT INTO "users" (
   "email",
   "password",
-  "teacher_id",
   "permissions",
   "created_at",
   "updated_at"
 )
 VALUES (
   'admin@school.com',
-  '$2b$10$Y8lgEDKSyBeXbXskPOy80ekkhhjraSUhI39itsBUQKM//QpmNAd6C',
-  NULL,
+  '$2a$10$q0t0DiviwiX4dxXQumC8V.jQ8.e1i.0wirNfR/n7fu8GPbXjLpw92',
   ARRAY[
-    'students:read',
-    'students:write',
-    'students:delete',
-    'teachers:read',
-    'teachers:write',
-    'teachers:delete',
-    'subjects:read',
-    'subjects:write',
-    'subjects:delete',
-    'class-offerings:read',
-    'class-offerings:write',
-    'class-offerings:delete',
-    'enrollments:read',
-    'enrollments:write',
-    'enrollments:delete',
-    'attendances:read',
-    'attendances:write',
-    'attendances:delete',
+    'associados:read',
+    'associados:write',
+    'associados:delete',
     'users:read',
     'users:write',
     'users:delete'
@@ -39,6 +22,5 @@ VALUES (
 ON CONFLICT ("email") DO UPDATE
 SET
   "password" = EXCLUDED."password",
-  "teacher_id" = EXCLUDED."teacher_id",
   "permissions" = EXCLUDED."permissions",
   "updated_at" = NOW();
